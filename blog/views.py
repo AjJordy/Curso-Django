97c3d950015b672527c3eaf8f250848dd7932102
AjJordy <jordyfaria0@gmail.com>
2023-05-20 12:05:01 -0300

    views

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,12 @@ from django.http import HttpResponse
 
 def blog(request):
 	print('blog')
-	return HttpResponse('blog do app')
+	return render(
+		request,
+		'blog/index.html'
+	)
+
+
 
 
 def exemplo(request):
--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
 	print('home')
-	return HttpResponse('home do app')
+	# return HttpResponse('home do app')
+	return render(
+		request,
+		'home/index.html'
+	)
 
