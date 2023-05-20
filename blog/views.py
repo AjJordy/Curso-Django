7d179c795b1efad506d9a5bdd80e4bc405dec9d0
AjJordy <jordyfaria0@gmail.com>
2023-05-20 11:36:50 -0300

    urls

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
-
+# blog/
 
 def blog(request):
 	print('blog')
 	return HttpResponse('blog do app')
+
+
+def exemplo(request):
+	print('exemplo')
+	return HttpResponse('exemplo do app')
--- project/urls.py
+++ project/urls.py
@@ -15,16 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from home import views as home_views
-from blog import views as blog_views
+from django.urls import path, include
 
 # HTTP Request <-> Response
 # MVT (MVC)
 
 urlpatterns = [    
-    path('', home_views.home), # Home
-    path('blog/', blog_views.blog),    
-    path('admin/', admin.site.urls),
+	path('', include('home.urls')), # path('', home_views.home),    
+	path('blog/', include('blog.urls')), # path('blog/', blog_views.blog),    
+    path('admin/', admin.site.urls), 
 ]
